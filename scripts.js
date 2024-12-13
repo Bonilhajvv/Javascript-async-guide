44eb454b8ce492f121a5d6aca48e81270aba06c6
Bonilhajvv <joaovictorbonilhaventurinib@gmail.com>
2024-12-13 17:54:48 -0300

    feat: script para add tags

--- scripts.js
+++ scripts.js
@@ -29,16 +29,35 @@ const nomeDaImagem = document.querySelector(".container-imagem-nome p");
 
 inputUpload.addEventListener("change", async (evento) => { //change: O evento é disparado quando o usuário seleciona um arquivo no input.
     const arquivo = evento.target.files[0];                 //evento.target.files: Retorna uma lista dos arquivos selecionados pelo usuário.[0]: 
-                                                            // Seleciona o primeiro arquivo da lista (caso o input permita múltiplos arquivos).
+    // Seleciona o primeiro arquivo da lista (caso o input permita múltiplos arquivos).
 
     if (arquivo) {
         try {
             const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo); //lerConteudoDoArquivo: Função previamente definida (como no exemplo anterior) que retorna uma Promise. 
-                                                                            // Ela lê o conteúdo do arquivo e o converte para uma URL codificada em base64.
+            // Ela lê o conteúdo do arquivo e o converte para uma URL codificada em base64.
             imagemPrincipal.src = conteudoDoArquivo.url;
             nomeDaImagem.textContent = conteudoDoArquivo.nome;
         } catch (erro) {
             console.error("Erro na leitura do arquivo")
+        }
+    }
+})
+
+const inputTags = document.getElementById("input-tags");
+const listaTags = document.getElementById("lista-tags");
+
+inputTags.addEventListener("keypress", (evento) => {
+    if (evento.key === "Enter") {
+        evento.preventDefault();            //usamos event.preventDefault() para evitar o comportamento padrão de atualização da tela ao pressionar "Enter",
+        // garantindo que não perderemos o que foi digitado no formulário.
+        const tagTexto = inputTags.value.trim(); //Para capturar o texto da tag, criamos uma constante tagTexto que recebe o valor do campo de entrada inputTags.value.trim(). 
+        //O método trim() remove espaços em branco antes e depois do texto.
+        if (tagTexto !== "") {
+            const tagNova = document.createElement("li"); //Prosseguindo, verificamos se há algo digitado em tagTexto com a condição if (tagTexto !== "").
+            // Se houver, criamos um novo item de lista para a tag com const tagNova = document.createElement("li").
+            tagNova.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`
+            listaTags.appendChild(tagNova);
+            inputTags.value = "";
         }
     }
 })
