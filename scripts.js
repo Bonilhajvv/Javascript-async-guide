da5d75d853ec5658d74fde9bc9589be55c663780
Bonilhajvv <joaovictorbonilhaventurinib@gmail.com>
2024-12-16 10:08:03 -0300

    feature: integração de algumas funções

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-//Promessas em JavaScript é um objeto que representa o sucesso ou a falha de uma operação assíncrona.
-//Uma função async é uma função que retorna uma promessa, 
-// e o await é usado para esperar por essa promessa ser resolvida (ou seja, completada) antes de continuar com o próximo passo.
-
 const uploadBtn = document.getElementById("upload-btn");
 const inputUpload = document.getElementById("image-upload")
 
@@ -10,31 +6,29 @@ uploadBtn.addEventListener("click", () => {
 })
 
 function lerConteudoDoArquivo(arquivo) {
-    return new Promise((resolve, reject) => {                           //Promisse: retorna algo que pode acontecer ou nao, se ira dar certo ou nao
-        const leitor = new FileReader();                                //leitor de arquivo
-        leitor.onload = () => {                                         //on load ocorre quando o arquivo é lido com sucesso
-            resolve({ url: leitor.result, nome: arquivo.name })         //se der certo ele retorna 
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = () => {
+            resolve({ url: leitor.result, nome: arquivo.name })
         }
 
-        leitor.onerror = () => {                                         //ocorre quando o arquivo não é lido com sucesso
-            reject(`Erro na leitura do arquivo ${arquivo.name}`)        //se der errado ele retorna isso
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`)
         }
 
-        leitor.readAsDataURL(arquivo)                                     //le o arquivo e converte o arquivo para uma string
+        leitor.readAsDataURL(arquivo)
     })
 }
 
 const imagemPrincipal = document.querySelector(".main-imagem");
 const nomeDaImagem = document.querySelector(".container-imagem-nome p");
 
-inputUpload.addEventListener("change", async (evento) => { //change: O evento é disparado quando o usuário seleciona um arquivo no input.
-    const arquivo = evento.target.files[0];                 //evento.target.files: Retorna uma lista dos arquivos selecionados pelo usuário.[0]: 
-    // Seleciona o primeiro arquivo da lista (caso o input permita múltiplos arquivos).
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];
 
     if (arquivo) {
         try {
-            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo); //lerConteudoDoArquivo: Função previamente definida (como no exemplo anterior) que retorna uma Promise. 
-            // Ela lê o conteúdo do arquivo e o converte para uma URL codificada em base64.
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
             imagemPrincipal.src = conteudoDoArquivo.url;
             nomeDaImagem.textContent = conteudoDoArquivo.nome;
         } catch (erro) {
@@ -46,18 +40,90 @@ inputUpload.addEventListener("change", async (evento) => { //change: O evento é
 const inputTags = document.getElementById("input-tags");
 const listaTags = document.getElementById("lista-tags");
 
-inputTags.addEventListener("keypress", (evento) => {
+inputTags.addEventListener("keypress", async (evento) => {
     if (evento.key === "Enter") {
-        evento.preventDefault();            //usamos event.preventDefault() para evitar o comportamento padrão de atualização da tela ao pressionar "Enter",
-        // garantindo que não perderemos o que foi digitado no formulário.
-        const tagTexto = inputTags.value.trim(); //Para capturar o texto da tag, criamos uma constante tagTexto que recebe o valor do campo de entrada inputTags.value.trim(). 
-        //O método trim() remove espaços em branco antes e depois do texto.
+        evento.preventDefault();
+        const tagTexto = inputTags.value.trim();
         if (tagTexto !== "") {
-            const tagNova = document.createElement("li"); //Prosseguindo, verificamos se há algo digitado em tagTexto com a condição if (tagTexto !== "").
-            // Se houver, criamos um novo item de lista para a tag com const tagNova = document.createElement("li").
-            tagNova.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`
-            listaTags.appendChild(tagNova);
-            inputTags.value = "";
+            try {
+                const tagExiste = await verificaTagsDisponiveis(tagTexto);
+                if (tagExiste) {
+                    const tagNova = document.createElement("li");
+                    tagNova.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`
+                    listaTags.appendChild(tagNova);
+                    inputTags.value = "";
+                } else {
+                    alert("Tag não foi encontrada.");
+                }
+            } catch (error) {
+                console.error("Erro ao verificar a existência da tag");
+                alert("Erro ao verificar a existência da tag. Verifique o console.")
+            }
         }
     }
 })
+
+listaTags.addEventListener("click", (evento) => {
+    if (evento.target.classList.contains("remove-tag")) {
+        const tagQueQueremosRemover = evento.target.parentElement;
+        listaTags.removeChild(tagQueQueremosRemover);
+    }
+})
+
+const tagsDisponiveis = ["Front-end", "Programação", "Data Science", "Full-stack", "HTML", "CSS", "JavaScript"];
+
+async function verificaTagsDisponiveis(tagTexto) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(tagsDisponiveis.includes(tagTexto));
+        }, 1000)
+    })
+}
+
+const botaoPublicar = document.querySelector(".botao-publicar");
+
+
+async function publicarProjeto(nomeDoProjeto, descricaoProjeto, tagsProjeto) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const deuCerto = Math.random() > 0.5;
+            if (deuCerto) {
+                resolve("Projeto publicado com sucesso.")
+            } else {
+                reject("Erro ao publicar o projeto.")
+            }
+        }, 2000)
+    })
+}
+
+botaoPublicar.addEventListener("click", async (evento) => {
+    evento.preventDefault();
+
+    const nomeDoProjeto = document.getElementById("nome").value;
+    const descricaoDoProjeto = document.getElementById("descricao").value;
+    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
+
+    try {
+        const resultado = await publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto);
+        console.log(resultado);
+        alert("Deu tudo certo")
+    }catch(error){
+        console.log("Deu errado: ", error);
+        alert("Deu tudo errado!");
+    }
+})
+
+const botaoDescartar = document.querySelector(".botao-descartar");
+
+botaoDescartar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+
+    const formulario = document.querySelector("form");
+    formulario.reset();
+
+    imagemPrincipal.src = "./img/imagem1.png";
+    nomeDaImagem.textContent = "image_projeto.png";
+
+    listaTags.innerHTML = "";
+})
+
